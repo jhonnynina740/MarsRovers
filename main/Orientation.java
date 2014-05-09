c3b43c9f3e7a44b6dd92ebbaea340d0ae9d06c59
jhonnynina740 <jhonnynina740@gmail.com>
2014-05-09 12:23:33 -0700

    Create Orientation.java

---
+++ main/Orientation.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Orientation {
+	public static final byte NORTH = 'N';
+	public static final byte SOUTH = 'S';
+	public static final byte EAST = 'E';
+	public static final byte WEST = 'W';	
+}
